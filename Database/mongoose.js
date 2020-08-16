15951869a67d7e9f160ebfe53390f26dd2d459f7
TEJA2312 <62765495+TEJA2312@users.noreply.github.com>
2020-08-17 01:39:11 +0530

    Delete mongoose.js

--- Database/mongoose.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose'); 
-mongoose.Promise = global.Promise
-
-mongoose.connect('mongodb+srv://tejasdevs:tejasdevs@cluster0.svck3.mongodb.net/<dbname>?retryWrites=true&w=majority',{ useNewUrlParser: true ,useFindAndModify: false, useUnifiedTopology: true });
-
-module.exports ={mongoose};
-
